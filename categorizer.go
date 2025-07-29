d5a41a74f8e4216f91e33d81159f5f659e571095
Hanna Shevchenko <hannashevchenko72@gmail.com>
2025-07-29 16:13:54 +0200

    add coverage for FizzBuzz category
    Signed-off-by: Hanna Shevchenko <hannashevchenko72@gmail.com>

--- categorizer.go
+++ categorizer.go
@@ -13,6 +13,9 @@ var Buzz = Category{value: "Buzz"}
 var FizzBuzz = Category{value: "FizzBuzz"}
 
 func Categorizer(number int) Category {
+	if number%3 == 0 && number%5 == 0 {
+		return FizzBuzz
+	}
 	if number%3 == 0 {
 		return Fizz
 	}
